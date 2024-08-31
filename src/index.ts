c3a89e5b183bfc9f2e6f2e06ba50ce5c99f26d94
Bento Netto <bentoeemanuele@gmail.com>
2024-08-30 22:29:18 -0300

    Criando a Estrutura inicial do projeto

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import 'dotenv/config'
 import express, { Request, Response } from 'express'
+import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
-app.get('/', (req: Request, res: Response)=>{
-    return res.json('Tudo ok!')
-})
+
+app.use(routes)
 
 app.listen(process.env.PORT, () =>{
     console.log('Servidor iniciado na porta: '+ process.env.PORT);
